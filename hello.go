30a1c48bb9f5a45efc41b60c6575b49ee09c7535
Lothar Schulz <lothar.sch@gmx.de>
2018-06-26 23:50:52 +0200

    handlefunc funcs switched

--- hello.go
+++ hello.go
@@ -45,8 +45,8 @@ func books(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	log.Printf("Server at http://localhost%v.\n", port)
 	http.HandleFunc("/", helloWorld)
 	http.HandleFunc("/magazine/", books)
+	log.Printf("Server at http://localhost%v.\n", port)
 	log.Fatal(http.ListenAndServe(port, nil))
 }
