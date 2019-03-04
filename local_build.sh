ba3e926f087af6812b79af958f7ebba8f9a59581
Lothar Schulz <lothar.schulz@moovel.com>
2019-03-04 18:11:15 +0100

    approaches seperated with comments

--- local_build.sh
+++ local_build.sh
@@ -3,18 +3,21 @@
 set -euo pipefail
 IFS=$'\n\t'
 
+# build.docker: build
 docker pull golang:1.12
 time make build.docker
 docker images
 docker rmi -f $(docker images -q)
 
+# build.docker-cache
 docker pull golang:1.12
 docker pull alpine:latest
 time make build.docker-cache
 docker images
 docker rmi -f $(docker images -q)
 
-# docker builder
+######## builder pattern
+# build.dockerbuilder
 docker pull golang:1.12
 docker pull alpine:latest
 
@@ -22,74 +25,83 @@ time make build.dockerbuilder
 docker images
 docker rmi -f $(docker images -q)
 
-
+# build.dockerbuilder-cache
 docker pull golang:1.12
 docker pull alpine:latest
 time make build.dockerbuilder-cache
 docker images
 docker rmi -f $(docker images -q)
 
+# build.dockerbuilder-nocache
 time make build.dockerbuilder-nocache
 docker images
 docker rmi -f $(docker images -q)
 
+# build.dockerbuilder-squash
 docker pull golang:1.12
 docker pull alpine:latest
 time make build.dockerbuilder-squash
 docker images
 docker rmi -f $(docker images -q)
 
+# build.dockerbuilder-compress
 docker pull golang:1.12
 docker pull alpine:latest
-
 time make build.dockerbuilder-compress
 docker images
 docker rmi -f $(docker images -q)
 
-# alpine
+######## alpine
+# build.dockeralpine
 docker pull golang:1.12
 docker pull alpine:latest
 time make build.dockeralpine
 docker images
 docker rmi -f $(docker images -q)
 
+# build.dockeralpine-cache
 docker pull golang:1.12
 docker pull alpine:latest
 time make build.dockeralpine-cache
 docker images
 docker rmi -f $(docker images -q)
 
+# build.dockeralpine-nocache
 time make build.dockeralpine-nocache
 docker images
 docker rmi -f $(docker images -q)
 
+# build.dockeralpine-squash
 docker pull golang:1.12
 docker pull alpine:latest
-
 time make build.dockeralpine-squash
 docker images
 docker rmi -f $(docker images -q)
 
+# build.dockeralpine-compress
 docker pull golang:1.12
 docker pull alpine:latest
-
 time make build.dockeralpine-compress
 docker images
 docker rmi -f $(docker images -q)
 
-# min
+######## min
+# build.docker-min
 time make build.docker-min
 docker images
 docker rmi -f $(docker images -q)
 
+# build.docker-min-nocache
 time make build.docker-min-nocache
 docker images
 docker rmi -f $(docker images -q)
 
+# build.docker-min-squash
 time make build.docker-min-squash
 docker images
 docker rmi -f $(docker images -q)
 
+# build.docker-min-compress
 time make build.docker-min-compress
 docker images
 docker rmi -f $(docker images -q)
